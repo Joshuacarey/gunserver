226d929a0bd52f071759cc8db55972e4c5206739
Stephen Martin <mediabytes@icloud.com>
2018-08-05 23:38:23 -0400

    bug fix and deploying server to heroku

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(Gun.serve);
 app.use(express.static(__dirname));
 var server = app.listen(port, () => console.log('Server started on port ' + port + ' with /gun'));
 Gun({	
-	file: 'data.json', 
+	file: 'gun', 
 	web: server 
 });
