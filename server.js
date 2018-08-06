c927ef4fdf5035259a2afe0a036cd5b4003e4dc0
Stephen Martin <mediabytes@icloud.com>
2018-08-05 23:05:24 -0400

    Deploying to Heroku

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || 8765;
 var express = require('express');
 var Gun = require('Gun');
-
 var app = express();
 app.use(Gun.serve);
 app.use(express.static(__dirname));
-
 var server = app.listen(port, () => console.log('Server started on port ' + port + ' with /gun'));
-
 Gun({	
 	file: 'data.json', 
 	web: server 
