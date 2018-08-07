a4eda491a1cf92d49ffab10e12d55979b5577cbb
Stephen Martin <mediabytes@icloud.com>
2018-08-06 22:15:07 -0400

    New server code

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || 8765;
+console.log("If modules not found, run `npm install` in /example folder!");
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765;
+var host = process.env.OPENSHIFT_NODEJS_HOST || process.env.VCAP_APP_HOST || process.env.HOST || 'localhost';
+
 var express = require('express');
-var Gun = require('Gun');
+var proxy = require('express-http-proxy');
+var http = require('http');
 var app = express();
-app.use(Gun.serve);
-app.use(express.static(__dirname));
-var server = app.listen(port, () => console.log('Server started on port ' + port + ' with /gun'));
-Gun({	
-	file: 'gun', 
-	web: server 
+var server = http.createServer(app);
+
+var Gun = require('gun');
+var gun = Gun({
+	file: 'data.json',
+	web: server
 });
+
+app.use(Gun.serve);
+app.use(proxy(host + ':8765'));
+server.listen(port);
+
+console.log('Server started on port ' + port + ' with /gun');
