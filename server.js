9382ff392e55a62719dedd607982aa614fee8361
mediabytes <mediabytes@icloud.com>
2018-08-07 22:18:49 -0400

    code clean up

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
-console.log("If modules not found, run `npm install` in /example folder!");
-var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765;
-var host = process.env.OPENSHIFT_NODEJS_HOST || process.env.VCAP_APP_HOST || process.env.HOST || 'localhost';
-
-var express = require('express');
-var proxy = require('express-http-proxy');
-var http = require('http');
-var app = express();
-var server = http.createServer(app);
-
-var Gun = require('gun');
-var gun = Gun({
-	file: 'data.json',
-	web: server
-});
-
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765;
+const host = process.env.OPENSHIFT_NODEJS_HOST || process.env.VCAP_APP_HOST || process.env.HOST || 'localhost';
+const express = require('express');
+const proxy = require('express-http-proxy');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const Gun = require('gun');
+const gun = Gun({ file: 'data.json', web: server });
 app.use(Gun.serve);
 app.use(proxy(host + ':8765'));
 server.listen(port);
-
 console.log('Server started on port ' + port + ' with /gun');
